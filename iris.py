69987261531579f95389cb3e453e430f42ef0fdd
Collin Sullivan <cs@hordelacrosse.com>
2017-03-03 13:24:54 -0600

    Create iris.py

---
+++ iris.py
@@ -0,0 +1,72 @@
+
+
+#Imports_____________________________________________________________________________________________
+
+import matplotlib.pyplot as plt 
+import numpy as np
+import pandas as pd 
+
+
+#Perceptron Algorithm Class__________________________________________________________________________
+
+class Perceptron(object):
+
+    def __init__(self, eta=0.01, epochs=50):
+        self.eta = eta
+        self.epochs = epochs
+
+    def fit(self, X, y):
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.epochs):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] +=  update * xi
+                self.w_[0] +=  update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+
+#Display Petal Graph__________________________________________________________________________________
+ 
+df = pd.read_csv('iris.data', header=None)
+df.tail()
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+
+plt.scatter(X[:50, 0], X[:50, 1], color='green', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='o', label='versicolor')
+
+plt.xlabel('sepal length')
+plt.ylabel('petal length')
+
+plt.legend(loc='upper left')
+plt.show()
+
+
+
+#Train and Show Data__________________________________________________________________________________
+
+ppn = Perceptron(eta=0.1, epochs=10)
+
+ppn.fit(X, y)
+
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+
+plt.show()
