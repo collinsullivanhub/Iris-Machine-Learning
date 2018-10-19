7e38e80906eee428b62c459f80dd1b8061c30f64
Collin Sullivan <cs@hordelacrosse.com>
2018-10-19 01:59:35 +0000

    Update iris.py

--- iris.py
+++ iris.py
@@ -44,10 +44,8 @@ X = df.iloc[0:100, [0, 2]].values
 
 plt.scatter(X[:50, 0], X[:50, 1], color='green', marker='o', label='setosa')
 plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='o', label='versicolor')
-
 plt.xlabel('sepal length')
 plt.ylabel('petal length')
-
 plt.legend(loc='upper left')
 plt.show()
 
